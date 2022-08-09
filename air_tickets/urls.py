49f945c0f2788bfad9b7ffc8ce23c447a7c25427
Ertayx <ertaiesen05@gmail.com>
2022-08-09 12:13:56 +0600

    urls

---
+++ air_tickets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from .views import TicketViewSet, OrderViewSet
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+
+router.register('orders', OrderViewSet, basename='orders')
+router.register('tickets', TicketViewSet, basename='tickets')
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- air_tickets/views.py
+++ air_tickets/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import mixins
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .models import AirTic, Order
+from .permissions import IsAuthor
+from .serializers import AirTicSerializer, OrderSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class TicketViewSet(ModelViewSet, GenericViewSet):
+
+    queryset = AirTic.objects.all()
+    serializer_class = AirTicSerializer
+
+
+class OrderViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, GenericViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated, IsAuthor]
